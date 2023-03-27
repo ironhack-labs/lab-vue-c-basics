89cdb5b2d86136ca271c962160fb305f0e3edbfd
Yasser Santiago <yasser.santiagoe@gmail.com>
2023-03-27 17:27:43 +0200

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import headerSect from './components/headerSect.vue';
+import footerSect from './components/footerSect.vue';
+</script>
+
+<template>
+<header>
+    <headerSect />
+</header>
+
+<footer>
+    <footerSect />
+</footer>
+</template>
+
+<style scoped>
+    header {
+        position: sticky;
+    }
+</style>
