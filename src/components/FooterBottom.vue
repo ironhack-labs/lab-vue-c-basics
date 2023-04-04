81246e4f28cc2a23a297db618ce5ca0c52dcda12
Pol Cadavid <polkadavid@gmail.com>
2023-04-04 18:03:36 +0200

    done

---
+++ src/components/FooterBottom.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>Footer</h1>
+    <ul>
+      <li v-for="(post, id) in posts" :key="id">
+        <h2>{{ post.title }}</h2>
+        <h4>{{ post.description }}</h4>
+        <p>{{ post.content }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FooterBottom',
+  data() {
+    return {
+      posts: [
+        {
+          title: 'First Post',
+          description: 'This is the first Post',
+          content: 'Carefully written text for the first post'
+        },
+        {
+          title: 'Second Post',
+          description: 'This is the second Post',
+          content: 'Carefully written text for the second post'
+        },
+        {
+          title: 'Third Post',
+          description: 'This is the third Post',
+          content: 'Carefully written text for the third post'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped></style>
---
+++ src/components/NavbarTop.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <h1 :class="{ navbarRed: isRed, navbarBlue: isBlue }">Navbar</h1>
+  </div>
+  <button @click="changeColor()">Change Navbar color</button>
+  <div>
+    <p>{{ math }}</p>
+  </div>
+  <div v-if="bitcoin">
+    <h2>Bitcoin is king</h2>
+    <div>
+      <img class="btc-image" src="../assets/Bitcoin.jpg" alt="Bitcoin image" />
+    </div>
+    <button @click="revealTheTruth()">What you say?</button>
+  </div>
+  <div v-else>
+    <h2>Bitcoin is a scam</h2>
+    <button @click="revealTheTruth()">Reveal The Truth</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavbarTop',
+  props: {
+    math: {
+      type: Number
+    },
+    bitcoin: {
+      type: Boolean,
+      default: false
+    },
+    isRed: {
+      type: Boolean,
+      default: false
+    },
+    isBlue: {
+      type: Boolean,
+      default: true
+    }
+  },
+  methods: {
+    revealTheTruth() {
+      this.$emit('changeBitcoin')
+    },
+    changeColor() {
+      this.$emit('changeColor')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.navbarRed {
+  background-color: red;
+}
+.navbarBlue {
+  background-color: blue;
+}
+.btc-image {
+  width: 300px;
+}
+</style>
