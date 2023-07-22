8b584fb31998d25baafff77d518b494654a62c94
Felipe Hanitzsch <felipe.hanitzsch@gmail.com>
2023-07-22 12:38:31 +0200

    done by Hanitzsch

---
+++ vue-project/src/App.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { ref } from 'vue';
+
+const numero = (2);
+const numerodois = (2);
+var resultado = ref('0');
+const fundo =ref("fundo");
+
+const soma = function() {
+  resultado.value = numero + numerodois;
+  console.log(resultado);
+}
+
+const troca = function () {
+ fundo.value = ("cor");
+}
+
+const reset = function () {
+  fundo.value = ("fundo");
+  resultado.value = ("0");
+  
+}
+
+</script>
+
+<template>
+
+  <div :class="fundo">
+  <button @click="soma">Soma</button>
+  <p>{{ resultado }}</p>
+  <p v-if="resultado == 4">boolean Funcionou!</p>
+<button @click="troca">troca de cor</button>
+<button @click="reset">Reset</button>
+
+</div>
+</template>
+
+<style>
+
+.fundo {
+  background-color: aqua;
+}
+
+.cor {
+  background-color: blueviolet;
+}
+
+</style>
