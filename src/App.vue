d99af2b7597572e7870c5974d72c602a92dcb9d8
Anna <ania.pwk.ap@gmail.com>
2024-03-09 17:46:53 +0100

    done

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,33 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import NavbarComponent from './components/NavbarComponent.vue'
+import FooterComponent from './components/FooterComponent.vue'
+import { ref } from 'vue'
+
+const firstName = ref('John')
+const lastName = ref('Doe')
+
+// Step 2: Function to generate a string
+const generateString = () => {
+  return `${firstName.value} ${lastName.value}`
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div>
+    <p>Result of 2 + 2 is: {{ 2 + 2 }}</p>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+    <!-- Step 2: Display String Returned by a Function -->
+    <p>{{ generateString() }}</p>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <NavbarComponent />
+    <FooterComponent />
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+div {
+  display: flex;
+  flex-direction: column;
+  min-width: 500px;
 }
 </style>
