3a3f4c5ef1ac8a23972153bfb8269d9f9d32008f
Joan Vila Valls <joan.o.jan@gmail.com>
2024-03-10 11:13:15 +0100

    navbar background color random

--- src/App.vue
+++ src/App.vue
@@ -8,16 +8,24 @@ import Posts from "@/components/Posts.vue"
 // reactive state
 const greeting = ref("Hello, ")
 const name = "My Name is Foo"
+const backgroundColor = ref("green")
 
 
 function greet() {
   return `${greeting.value + name}`
 }
 
+function changeNavbarBackgroundColor(){
+  let randomColor = Math.floor(Math.random()*16777215).toString(16)
+  backgroundColor.value = "#" + randomColor
+}
+
 </script>
 
 <template>
-  <Navbar />
+  <Navbar :background-color=backgroundColor />
+  <button @click="changeNavbarBackgroundColor">Click me</button>
+  <BackgroundChangeButton />
   <h1>{{ greet() }}</h1><br>
   <p>This is the mathematical operation 2 + 2 = {{ 2 + 2 }}</p>
   <Conditional />
