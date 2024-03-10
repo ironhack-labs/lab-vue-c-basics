fa14acceedd8f8efbc74859118e05c1e1f898b23
Joan Vila Valls <joan.o.jan@gmail.com>
2024-03-10 07:43:09 +0100

    v-for, rendering posts, Posts component

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -34,6 +34,34 @@ console.log(posts)
 
 <template>
   <ul>
-    <li v-for="post in posts" :key="post.id">{{ post.title }}</li>
+    <li v-for="post in posts" :key="post.id">
+      <h3> {{ post.title }} </h3>
+      <h4> {{ post.description }} </h4>
+      <p> {{ post.content }}</p>
+    </li>
   </ul>
 </template>
+
+<style scoped>
+ul {
+  list-style: none;
+}
+li {
+  background-color: aquamarine;
+  margin: 3rem;
+  padding: 3rem;
+  text-align: center;
+}
+h3, h4 {
+  font-weight: bold;
+  margin-bottom: 1rem;
+}
+h3 {
+  color: purple;
+  font-size: 2rem;
+}
+p::before, p::after {
+  content: " === "
+};
+
+</style>
