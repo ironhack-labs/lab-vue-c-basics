7ada90c5209b0631624402e4f8eec21ff849a103
Auba Fuster <aubafp@gmail.com>
2024-03-12 00:01:21 +0100

    Iteration 5 done

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@
     import FooterComp from "@/components/FooterComp.vue";
     import TextsOperationsComp from "@/components/TextsOperationsComp.vue";
     import ListOfPostsComp from "@/components/ListOfPostsComp.vue";
+    import ButtonComp from "@/components/ButtonComp.vue";
 </script>
 
 <template>
   <NavbarComp/> <!-- Iteration 1 & 3 -->
   <TextsOperationsComp/> <!-- Iteration 2 -->
   <ListOfPostsComp/> <!-- Iteration 4 -->
+  <ButtonComp/> <!-- Iteration 5 -->
   <FooterComp/> <!-- Iteration 1 -->
 </template>
 
