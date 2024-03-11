d42a4fbfe5037d83bafe7cbb8770b3d9de0a8cbe
Anna <ania.pwk.ap@gmail.com>
2024-03-11 23:41:45 +0100

    add compute

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <script setup>
 import NavbarComponent from './components/NavbarComponent.vue'
 import FooterComponent from './components/FooterComponent.vue'
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
 const firstName = ref('John')
 const lastName = ref('Doe')
 
 // Step 2: Function to generate a string
-const generateString = () => {
+const fullName = computed(() => {
   return `${firstName.value} ${lastName.value}`
-}
+})
 </script>
 
 <template>
@@ -17,7 +17,7 @@ const generateString = () => {
     <p>Result of 2 + 2 is: {{ 2 + 2 }}</p>
 
     <!-- Step 2: Display String Returned by a Function -->
-    <p>{{ generateString() }}</p>
+    <p>{{ fullName }}</p>
 
     <NavbarComponent />
     <FooterComponent />
