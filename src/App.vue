dce1fa13101b99268b03cc4b8172ece816efb56b
alexfreixa <alexfreixa18@gmail.com>
2024-03-13 00:21:53 +0100

    Lab solved

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,63 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import Navbar from './components/Navbar.vue';
+import TheFooter from './components/TheFooter.vue';
+
+import { reactive, ref } from 'vue'
+
+const author = reactive({
+  name: 'John Doe',
+  books: [
+    'Vue 2 - Advanced Guide',
+    'Vue 3 - Basic Guide',
+    'Vue 4 - The Mystery'
+  ]
+})
+
+const dog = reactive({
+  name: 'Tobby',
+  color: 'brown'
+})
+
+const ppl = ref(["Alex", "Joan", "Marc", "Ignasi", "Albert"]);
+
+function getCurrentAuthorName(objAuthor) {
+  return `The author's name is ${objAuthor.name}`
+}
+
+function onClick(){
+  const body = document.getElementById("app");
+  body.setAttribute("style", "background-color:red");
+}
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <Navbar></Navbar>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <div>2 + 2 = {{ 2+2 }}</div>
+  <div>{{ getCurrentAuthorName(author) }}</div>
+  <div v-if="dog.color === 'brown'">The dog is brown.</div>
+  <div>
+    List of persons
+    <ul>
+      <li v-for="p in ppl">{{ p }}</li>
+    </ul>
+  </div>
+  <div><a href="#" class="btn" @click="onClick">Change color</a></div>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <TheFooter></TheFooter>
+
 </template>
 
 <style scoped>
+
+.btn {
+  background-color: red;
+  padding: 10px 20px;
+}
+
 header {
   line-height: 1.5;
 }
---
+++ src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<script setup>
+/* defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+}) */
+</script>
+
+<template>
+
+    <span class="green">NAV</span>
+
+</template>
+
+<style scoped>
+
+</style>
