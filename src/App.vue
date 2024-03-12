57313e1db3807c7c0d2465feb21a64e4c7bd1229
MontseRPA <montse.pala@hotmail.com>
2024-03-12 16:14:29 +0100

    lab ended

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
 <script setup>
+
+import NavBarC from '@/assets/components/NavBarC.vue'
+import FooterC from '@/assets/components/FooterC.vue'
+import IterationsC from './assets/components/IterationsC.vue';
+
+
 </script>
 
 <template>
+
+  <NavBarC/>
+  <IterationsC/>
+  <FooterC/>
+
+  
 </template>
 
 <style scoped>
+
+
 </style>
---
+++ src/assets/components/FooterC.vue
@@ -0,0 +1,39 @@
+<script setup>
+
+import { ref } from "vue"; 
+
+const aeropuertosGrandes  = ref([
+  { title: `Adolfo Suárez Madrid-Barajas`,
+    description: `LEMD`,
+    content: `Pasajeros anuales: 60.220.984`,
+  },
+  { title: `Josep Tarradellas Barcelona-El Prat`,
+    description: `LEBL`,
+    content: `Pasajeros anuales: 49.909.544 `,
+  },
+  { title: `Palma de Mallorca`,
+    description: `LEPA`,
+    content: `Pasajeros anuales: 31.105.987`,
+  },
+])
+
+
+</script>
+
+<template>
+  <footer>
+
+    <ul>
+        <li v-for="character in aeropuertosGrandes" :key="character.title">
+            {{ `${character.title}. Código OACI: ${character.description}. ${character.content}` }}
+        </li>
+    </ul>
+
+    <p>Esto es el final</p>
+  </footer>
+
+  
+</template>
+
+<style scoped>
+</style>
