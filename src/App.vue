ae7aae73e1a0d5e2a3cea27f60eae4e700959daa
Max Freixa <freixa.sureda.max@alumnat.copernic.cat>
2024-03-12 23:40:02 +0100

    Iteration 1

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,14 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Navbar from "./components/navbar.vue"
+import TheFooter from "./components/footer.vue"
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
+  <Navbar />
   <main>
     <TheWelcome />
   </main>
+  <TheFooter />
 </template>
 
 <style scoped>
