0a69350b473ccb90ffea2b08880253a6759a90f6
Stefano Romanelli <stefanoromanelli86@gmail.com>
2024-03-26 20:38:15 +0100

    iterations 1 through 5

---
+++ src/components/NavBar.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <nav>
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/ToggleColor.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+      <button @click="toggleClass">Toggle Background</button>
+      <div :class="{ 'highlight': isHighlighted }" class="container">Container</div>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  
+  const isHighlighted = ref(false);
+  
+  function toggleClass() {
+    isHighlighted.value = !isHighlighted.value;
+  }
+  </script>
+  
+  <style>
+  .highlight {
+    background-color: green;
+    color: white;
+  }
+
+  .container {
+    margin: 20px;
+    border: 1px solid black;
+    padding: 10px;
+  }
+  </style>
