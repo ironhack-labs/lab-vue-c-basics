68034d39a77c92fd5648f3d99874e71fe5b4a3b9
Elisabetta Scinto <elisabettascinto@Elisabettas-Air.home>
2024-04-30 16:42:04 +0200

    done

---
+++ lab1-vue-project/src/components/Footer-comp.vue
@@ -0,0 +1,26 @@
+<script setup>
+import {ref} from 'vue'
+const footerItems = ref (['Home','About','Services','Contact'])
+</script>
+
+<template>
+  <nav>
+    <ul class="footer">
+      <li v-for="element in footerItems" :key="element">{{element}}</li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped>
+.footer{
+  display:flex;
+  flex-direction: row;
+  align-content: space-between;
+  gap: 50px;
+  list-style-type: none;
+  padding-left: 200px;
+  padding-top: 95%;
+  padding-bottom: 50px;
+  color:rgb(168, 162, 162)
+}
+</style>
