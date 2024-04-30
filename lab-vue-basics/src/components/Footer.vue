d6bef3ad37c1bc5f42645ec35e90e8112afa1bb0
Carol-88 <gralleta@gmail.com>
2024-04-30 13:32:20 +0200

    0 to iteration2

---
+++ lab-vue-basics/src/components/Footer.vue
@@ -0,0 +1,8 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup></script>
+<template>
+    <section>
+        <p>Footer</p>
+    </section>
+</template>
+<style scoped></style>
---
+++ lab-vue-basics/src/components/Navbar.vue
@@ -0,0 +1,37 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup>
+import { ref, reactive, computed} from 'vue'
+    const templateNavbar = reactive({
+        name: "textNav",
+        links: [
+            'home',
+            'about',
+            'contact'
+        ]
+    })
+
+    const viewLinks = computed(() => {
+        return templateNavbar.links;
+    })
+
+    const myName = ref("Carolina");
+    const lastName= ref("Romero");
+
+    const fullName = computed({
+        get() {
+            return myName.value + ' ' + lastName.value
+        }
+    })
+</script>
+<template>
+  <section>
+    <h1>{{templateNavbar.name}}</h1>
+    <p>{{2+2}}</p>
+    <ul id='navlinks'>
+       <li v-for="(link, index) in viewLinks" :key="index">{{link}}</li>
+    </ul>
+    <p>{{fullName}}</p>
+
+  </section>
+</template>
+<style scoped></style>
