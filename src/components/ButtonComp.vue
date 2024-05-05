c2ba79f13a2a6c6a9e039d03ab06e8b766d66170
JaumeBlond <46705356+JaumeBlond@users.noreply.github.com>
2024-05-05 21:08:36 +0200

    Lab solved

---
+++ src/components/ButtonComp.vue
@@ -0,0 +1,24 @@
+<script setup>
+    import { ref, provide } from 'vue'
+    const isLogoVisible = ref(false)
+
+    function showButtonText(isLogoVisible) {
+        return isLogoVisible ? 'Hide logo' : 'Show logo'
+    }
+</script>
+
+<template>
+    <button @click="isLogoVisible=!isLogoVisible" :class="{'green-bg': !isLogoVisible, 'red-bg': isLogoVisible}">{{ showButtonText(isLogoVisible) }}<img v-show="isLogoVisible" src="../assets/logo.svg"></button>
+</template>
+
+<style scoped>
+    .green-bg {
+        background-color: #88d888;
+        border: none;
+    }
+
+    .red-bg {
+        background-color: hsl(0, 49%, 65%);
+        border: none;
+    }
+</style>
---
+++ src/components/TextsOperationsComp.vue
@@ -0,0 +1,26 @@
+<script setup>
+    import { reactive } from 'vue' 
+
+    const objVoleiTeam = reactive({
+      name: 'DSB',
+      category: '3',
+      players: [
+        'p1',
+        'p2',
+        'p3',
+        'p4',
+        'p5',
+        'p6'
+      ],
+      winner: true
+    })
+
+    function getNumberOfPlayers(objTeam) {
+      return `The number of players of your team is ${objTeam.players.length}`
+    }
+</script>
+
+<template>
+  <p>The resulting operation for iteration 2.1 is: {{ 2+2 }}</p>
+  <p>The resulting operation for iteration 2.2 is: {{ getNumberOfPlayers(objVoleiTeam) }}</p>
+</template>
